6b4a61d03265324622a15a40306fdc550d5ef0cc
Django Exam <exam@example.com>
2026-04-04 11:05:10 +0600

    Fix delete function with simple confirm dialog

--- inventory_system/settings.py
+++ inventory_system/settings.py
@@ -88,6 +88,10 @@ USE_TZ = True
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
+# Static files
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 # Default primary key field type
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
